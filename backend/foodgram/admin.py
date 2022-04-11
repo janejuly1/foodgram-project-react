3334f3c2e9ecdbeef1929667817bdf134b95dd90
Janejuly1 <janejuly1@gmail.com>
2022-04-12 00:08:55 +0300

    AdminFilter v1.1

--- backend/foodgram/admin.py
+++ backend/foodgram/admin.py
@@ -1,6 +1,51 @@
 from django.contrib import admin
 
-from .models import Recipe, Ingredient
+from .models import Recipe, Ingredient, Tag
 
-admin.site.register(Recipe)
-admin.site.register(Ingredient)
+
+class RecipeAdmin(admin.ModelAdmin):
+    list_display = (
+        'pk',
+        'author_username',
+        'name',
+    )
+    list_filter = ('author__username', 'name', 'tags__name')
+    readonly_fields = ['recipe_fav_count']
+
+    def author_username(self, obj):
+        return obj.author.username
+
+    def tags_name(self, obj):
+        tags_name = []
+        for tag in obj.tags.all():
+            tags_name.append(tag.name)
+
+        return ', '.join(tags_name)
+
+    def recipe_fav_count(self, obj):
+        return obj.favourites.count()
+
+
+class IngredientAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'unit',
+    )
+    list_filter = ('name',)
+
+class TagAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'slug',
+    )
+    fields = (
+        'name',
+        'color_code',
+        'slug',
+    )
+
+admin.site.register(Recipe, RecipeAdmin)
+# admin.site.register(Recipe)
+admin.site.register(Ingredient, IngredientAdmin)
+# admin.site.register(Ingredient)
+admin.site.register(Tag, TagAdmin)
--- backend/user/admin.py
+++ backend/user/admin.py
@@ -2,4 +2,15 @@ from django.contrib import admin
 
 from .models import User
 
-admin.site.register(User)
+class UserAdmin(admin.ModelAdmin):
+    # list_display = (
+    #     'username',
+    #     'first_name',
+    #     'last_name',
+    #     'email',
+    #     'bio',
+    #     'role',
+    # )
+    list_filter = ('email', 'username')
+
+admin.site.register(User, UserAdmin)
