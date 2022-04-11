f4422eab08450fbf7398f6530fbf0dbe7d9b715a
Janejuly1 <janejuly1@gmail.com>
2022-04-11 22:24:57 +0300

    RecipeFilter v1.1

--- backend/api/filters.py
+++ backend/api/filters.py
@@ -1,11 +1,14 @@
 import django_filters as df
+from django.db.models import Q
+
+
 from foodgram.models import Recipe
 
 
 class RecipeFilter(df.FilterSet):
     author = df.NumberFilter(field_name='author__id', lookup_expr='exact')
-    is_favorited = df.NumberFilter(method='_is_favorited', lookup_expr='isnotnull')
-    is_in_shopping_cart = df.NumberFilter(method='_is_in_shopping_cart', lookup_expr='isnotnull')
+    is_favorited = df.NumberFilter(method='_is_favorited')
+    is_in_shopping_cart = df.NumberFilter(method='_is_in_shopping_cart')
     tags = df.AllValuesMultipleFilter(field_name='tags__slug')
 
     def __init__(self, *args, **kwargs):
@@ -18,10 +21,16 @@ class RecipeFilter(df.FilterSet):
         super(RecipeFilter, self).__init__(*args, **kwargs)
 
     def _is_favorited(self, qs, name, value):
-        return qs.filter(favourites__user=self.current_user)
+        if value == 1:
+            return qs.filter(favourites__user=self.current_user)
+
+        return qs.filter(~Q(favourites__user=self.current_user))
 
     def _is_in_shopping_cart(self, qs, name, value):
-        return qs.filter(shopping_cart__user=self.current_user)
+        if value == 1:
+            return qs.filter(shopping_cart__user=self.current_user)
+
+        return qs.filter(~Q(shopping_cart__user=self.current_user))
 
     class Meta:
         model = Recipe
