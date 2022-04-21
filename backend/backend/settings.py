16e0b65aabcf3001a4d58c571352d295fdd03e66
Janejuly1 <janejuly1@gmail.com>
2022-04-21 16:57:20 +0300

    django static fixed

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -92,10 +92,10 @@ USE_L10N = True
 
 USE_TZ = True
 
-STATIC_URL = '/static/'
+STATIC_URL = '/admin/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-MEDIA_URL = '/media/'
+MEDIA_URL = '/admin/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 REST_FRAMEWORK = {
